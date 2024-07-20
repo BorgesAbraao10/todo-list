0226a25a7adfff772af9d14d274a0e2badd37b91
BorgesAbraao10 <abraao10clear@gmail.com>
2024-07-20 20:28:47 -0300

    Last update

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,17 +1,61 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import styles from "./styles.module.scss";
 
+interface Task {
+  title: string;
+  done: boolean;
+  id: number;
+}
+
 export const Tasks: React.FC = () => {
-    const [taskTitle, setTas] = useState("")
+  const [taskTitle, setTaskTitle] = useState("");
+  const [task, setTasks] = useState([] as Task[]);
+
+  // Funçao disparada quando o usuário está querendo adicionar uma nova tarefa
+
+  function handleSubmitAddTask(event: FormEvent) {
+    event.preventDefault();
+
+    if (taskTitle.length < 3) {
+      alert("Não é possivel adicionar uma tarefa com menos de 3 letras.");
+      return;
+    }
+
+    // adicione a tarefa
+
+    setTasks([
+      ...task, // pega todas as tarefas que já existam e coloca nesse novo valor do estado de tarefas
+      { id: new Date().getTime(), title: taskTitle, done: false },
+    ]);
+    setTaskTitle("");
+  }
+
   return (
     <section className={styles.container}>
-      <form>
+      <form onSubmit={handleSubmitAddTask}>
         <div>
           <label htmlFor="task-title">Adicionar tarefas</label>
-          <input type="text" id="task-title" placeholder="Título da Tarefa" />
+          <input
+            value={taskTitle}
+            onChange={(event) => setTaskTitle(event.target.value)}
+            type="text"
+            id="task-title"
+            placeholder="Título da Tarefa"
+          />
         </div>
-        <button>Adicionar Tarefas</button>
+        <button type="submit">Adicionar Tarefas</button>
       </form>
+
+      <ul>
+        {task.map((task) => {
+          return (
+            <li key={task.id}>
+              <input type="checkbox" id={`task-${task.id}`} />
+              <label htmlFor={`task-${task.id}`}>{task.title}</label>
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 };
